cf29a5445449a7125ccc6bf79bbb15e86f66bb1e
ChrisR68 <flecks.lava_0y@icloud.com>
2026-09-14 22:58:18 +0200

    Update NixOS configuration on 2026-09-14 22:58:16

--- flake.nix
+++ flake.nix
@@ -42,6 +42,7 @@
           ./hosts/nixos/default.nix
           {
             networking.hostName = "nixos";
+            home-manager.users.chh = { imports = [ ./hosts/nixos/home.nix ]; };
           }
         ];
       };
@@ -54,11 +55,10 @@
           ./hosts/Gen2/default.nix
           {
             networking.hostName = "Gen2";
+            home-manager.users.chh = { imports = [ ./hosts/Gen2/home.nix ]; };
           }
         ];
       };
     };
   };
 }
-
-
---
+++ hosts/nixos/home.nix
@@ -0,0 +1,5 @@
+{ config, pkgs, ... }:
+
+{
+  # VM/Desktop-specific overrides for user chh
+}
